2f2d0213c55bb72e807c14c12a3caabd72aa3924
kagawagao <kingsongao1221@gmail.com>
2020-03-14 22:19:15 +0800

    fix(types): remove type export to fix no export warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,36 @@
-export { default as LineChart, LineChartProps } from './plots/line'
-export { default as StepLineChart, StepLineChartProps } from './plots/step-line'
-export { default as ScatterChart, ScatterChartProps } from './plots/scatter'
-export { default as ColumnChart, ColumnChartProps } from './plots/column'
-export {
-  default as HistogramChart,
-  HistogramChartProps,
-} from './plots/histogram'
-export {
-  default as WaterfallChart,
-  WaterfallChartProps,
-} from './plots/waterfall'
-export {
-  default as StackColumnChart,
-  StackColumnChartProps,
-} from './plots/stack-column'
-export {
-  default as RangeColumnChart,
-  RangeColumnChartProps,
-} from './plots/range-column'
-export {
-  default as PercentageStackColumnChart,
-  PercentageStackColumnChartProps,
-} from './plots/percentage-stack-column'
-export {
-  default as GroupColumnChart,
-  GroupColumnChartProps,
-} from './plots/group-column'
-export { default as BarChart, BarChartProps } from './plots/bar'
-export { default as StackBarChart, StackBarChartProps } from './plots/stack-bar'
-export { default as RangeBarChart, RangeBarChartProps } from './plots/range-bar'
-export {
-  default as PercentageStackBarChart,
-  PercentageStackBarChartProps,
-} from './plots/percentage-stack-bar'
-export { default as GroupBarChart, GroupBarChartProps } from './plots/group-bar'
-export { default as AreaChart, AreaChartProps } from './plots/area'
-export { default as DensityChart, DensityChartProps } from './plots/density'
-export {
-  default as StackAreaChart,
-  StackAreaChartProps,
-} from './plots/stack-area'
-export {
-  default as PercentageStackAreaChart,
-  PercentageStackAreaChartProps,
-} from './plots/percentage-stack-area'
-export { default as BubbleChart, BubbleChartProps } from './plots/bubble'
-export { default as PieChart, PieChartProps } from './plots/pie'
-export { default as RingChart, RingChartProps } from './plots/ring'
-export { default as RadarChart, RadarChartProps } from './plots/radar'
-export { default as GaugeChart, GaugeChartProps } from './plots/gauge'
-export { default as HeatmapChart, HeatmapChartProps } from './plots/heatmap'
-export { default as MatrixChart, MatrixChartProps } from './plots/matrix'
-export { default as ProgressChart, ProgressChartProps } from './plots/progress'
-export {
-  default as RingProgressChart,
-  RingProgressChartProps,
-} from './plots/ring-progress'
-export { default as TinyLineChart, TinyLineChartProps } from './plots/tiny-line'
-export { default as TinyAreaChart, TinyAreaChartProps } from './plots/tiny-area'
-export {
-  default as TinyColumnChart,
-  TinyColumnChartProps,
-} from './plots/tiny-column'
-export { default as LiquidChart, LiquidChartProps } from './plots/liquid'
-export { default as FunnelChart, FunnelChartProps } from './plots/funnel'
-export { default as BulletChart, BulletChartProps } from './plots/bullet'
-export { default as TreemapChart, TreemapChartProps } from './plots/treemap'
-
-export {
-  default as OverlappedComboChart,
-  OverlappedComboPlotChartProps,
-} from './plots/overlapped-combo'
+export { default as LineChart } from './plots/line'
+export { default as StepLineChart } from './plots/step-line'
+export { default as ScatterChart } from './plots/scatter'
+export { default as ColumnChart } from './plots/column'
+export { default as HistogramChart } from './plots/histogram'
+export { default as WaterfallChart } from './plots/waterfall'
+export { default as StackColumnChart } from './plots/stack-column'
+export { default as RangeColumnChart } from './plots/range-column'
+export { default as PercentageStackColumnChart } from './plots/percentage-stack-column'
+export { default as GroupColumnChart } from './plots/group-column'
+export { default as BarChart } from './plots/bar'
+export { default as StackBarChart } from './plots/stack-bar'
+export { default as RangeBarChart } from './plots/range-bar'
+export { default as PercentageStackBarChart } from './plots/percentage-stack-bar'
+export { default as GroupBarChart } from './plots/group-bar'
+export { default as AreaChart } from './plots/area'
+export { default as DensityChart } from './plots/density'
+export { default as StackAreaChart } from './plots/stack-area'
+export { default as PercentageStackAreaChart } from './plots/percentage-stack-area'
+export { default as BubbleChart } from './plots/bubble'
+export { default as PieChart } from './plots/pie'
+export { default as RingChart } from './plots/ring'
+export { default as RadarChart } from './plots/radar'
+export { default as GaugeChart } from './plots/gauge'
+export { default as HeatmapChart } from './plots/heatmap'
+export { default as MatrixChart } from './plots/matrix'
+export { default as ProgressChart } from './plots/progress'
+export { default as RingProgressChart } from './plots/ring-progress'
+export { default as TinyLineChart } from './plots/tiny-line'
+export { default as TinyAreaChart } from './plots/tiny-area'
+export { default as TinyColumnChart } from './plots/tiny-column'
+export { default as LiquidChart } from './plots/liquid'
+export { default as FunnelChart } from './plots/funnel'
+export { default as BulletChart } from './plots/bullet'
+export { default as TreemapChart } from './plots/treemap'
+export { default as OverlappedComboChart } from './plots/overlapped-combo'
