704e5e9cc78ad0eb28cf43ab7fb1006e5adb4a37
kagawagao <kingsongao1221@gmail.com>
2020-03-19 22:25:35 +0800

    test: update test case

--- __tests__/plots/meter-gauge.spec.tsx
+++ __tests__/plots/meter-gauge.spec.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
-import FanGaugeChart from '../../src/plots/fan-gauge'
+import MeterGaugeChart from '../../src/plots/meter-gauge'
 
 describe('GaugeChart', () => {
   test('should render without crashed', () => {
-    const renderer = create(<FanGaugeChart stackField="X" />)
+    const renderer = create(<MeterGaugeChart />)
 
     expect(renderer.toJSON()).toMatchSnapshot()
   })
