2ab739b35ef9295b8a2934b5acf35ea1a60afbd5
kagawagao <kingsongao1221@gmail.com>
2020-05-14 10:38:11 +0800

    fix: fix empty export warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,115 +1,195 @@
 // base plots
-export { default as AreaChart, AreaChartProps } from './plots/area'
-export { default as BarChart, BarChartProps } from './plots/bar'
-export { default as BubbleChart, BubbleChartProps } from './plots/bubble'
-export { default as BulletChart, BulletChartProps } from './plots/bullet'
-export { default as CalendarChart, CalendarChartProps } from './plots/calendar'
-export { default as ColumnChart, ColumnChartProps } from './plots/column'
-export { default as DensityChart, DensityChartProps } from './plots/density'
-export {
-  default as DensityHeatmapChart,
-  DensityHeatmapChartProps,
-} from './plots/density-heatmap'
-export { default as DonutChart, DonutChartProps } from './plots/donut'
-export { default as FanGaugeChart, FanGaugeChartProps } from './plots/fan-gauge'
-export { default as FunnelChart, FunnelChartProps } from './plots/funnel'
-export { default as GaugeChart, GaugeChartProps } from './plots/gauge'
-export {
-  default as GroupedBarChart,
-  GroupedBarChartProps,
-} from './plots/grouped-bar'
-export {
-  default as GroupedColumnChart,
-  GroupedColumnChartProps,
-} from './plots/grouped-column'
-export {
-  default as GroupedRoseChart,
-  GroupedRoseChartProps,
-} from './plots/grouped-rose'
-export { default as HeatmapChart, HeatmapChartProps } from './plots/heatmap'
-export {
-  default as HistogramChart,
-  HistogramChartProps,
-} from './plots/histogram'
-export { default as LineChart, LineChartProps } from './plots/line'
-export { default as LiquidChart, LiquidChartProps } from './plots/liquid'
-export {
-  default as MeterGaugeChart,
-  MeterGaugeChartProps,
-} from './plots/meter-gauge'
-export {
-  default as PercentStackedAreaChart,
-  PercentStackedAreaChartProps,
-} from './plots/percent-stacked-area'
-export {
-  default as PercentStackedBarChart,
-  PercentStackedBarChartProps,
-} from './plots/percent-stacked-bar'
-export {
-  default as PercentStackedColumnChart,
-  PercentStackedColumnChartProps,
-} from './plots/percent-stacked-column'
-export { default as PieChart, PieChartProps } from './plots/pie'
-export { default as RadarChart, RadarChartProps } from './plots/radar'
-export { default as RangeBarChart, RangeBarChartProps } from './plots/range-bar'
-export {
-  default as RangeColumnChart,
-  RangeColumnChartProps,
-} from './plots/range-column'
-export { default as RingChart, RingChartProps } from './plots/ring'
-export { default as RoseChart, RoseChartProps } from './plots/rose'
-export { default as ScatterChart, ScatterChartProps } from './plots/scatter'
-export {
-  default as StackedAreaChart,
-  StackedAreaChartProps,
-} from './plots/stacked-area'
-export {
-  default as StackedBarChart,
-  StackedBarChartProps,
-} from './plots/stacked-bar'
-export {
-  default as StackedColumnChart,
-  StackedColumnChartProps,
-} from './plots/stacked-column'
-export {
-  default as StackedRoseChart,
-  StackedRoseChartProps,
-} from './plots/stacked-rose'
-export { default as StepLineChart, StepLineChartProps } from './plots/step-line'
-export { default as TreemapChart, TreemapChartProps } from './plots/treemap'
-export {
-  default as WaterfallChart,
-  WaterfallChartProps,
-} from './plots/waterfall'
-export {
-  default as WordCloudChart,
-  WordCloudChartProps,
-} from './plots/word-cloud'
+import { AreaChartProps as _AreaChartProps } from './plots/area'
+import { BarChartProps as _BarChartProps } from './plots/bar'
+import { BubbleChartProps as _BubbleChartProps } from './plots/bubble'
+import { BulletChartProps as _BulletChartProps } from './plots/bullet'
+import { CalendarChartProps as _CalendarChartProps } from './plots/calendar'
+import { ColumnChartProps as _ColumnChartProps } from './plots/column'
+import { DensityChartProps as _DensityChartProps } from './plots/density'
+import { DensityHeatmapChartProps as _DensityHeatmapChartProps } from './plots/density-heatmap'
+import { DonutChartProps as _DonutChartProps } from './plots/donut'
+import { FanGaugeChartProps as _FanGaugeChartProps } from './plots/fan-gauge'
+import { FunnelChartProps as _FunnelChartProps } from './plots/funnel'
+import { GaugeChartProps as _GaugeChartProps } from './plots/gauge'
+import { GroupedBarChartProps as _GroupedBarChartProps } from './plots/grouped-bar'
+import { GroupedColumnChartProps as _GroupedColumnChartProps } from './plots/grouped-column'
+import { GroupedRoseChartProps as _GroupedRoseChartProps } from './plots/grouped-rose'
+import { HeatmapChartProps as _HeatmapChartProps } from './plots/heatmap'
+import { HistogramChartProps as _HistogramChartProps } from './plots/histogram'
+import { LineChartProps as _LineChartProps } from './plots/line'
+import { LiquidChartProps as _LiquidChartProps } from './plots/liquid'
+import { MeterGaugeChartProps as _MeterGaugeChartProps } from './plots/meter-gauge'
+import { PercentStackedAreaChartProps as _PercentStackedAreaChartProps } from './plots/percent-stacked-area'
+import { PercentStackedBarChartProps as _PercentStackedBarChartProps } from './plots/percent-stacked-bar'
+import { PercentStackedColumnChartProps as _PercentStackedColumnChartProps } from './plots/percent-stacked-column'
+import { PieChartProps as _PieChartProps } from './plots/pie'
+import { RadarChartProps as _RadarChartProps } from './plots/radar'
+import { RangeBarChartProps as _RangeBarChartProps } from './plots/range-bar'
+import { RangeColumnChartProps as _RangeColumnChartProps } from './plots/range-column'
+import { RingChartProps as _RingChartProps } from './plots/ring'
+import { RoseChartProps as _RoseChartProps } from './plots/rose'
+import { ScatterChartProps as _ScatterChartProps } from './plots/scatter'
+import { StackedAreaChartProps as _StackedAreaChartProps } from './plots/stacked-area'
+import { StackedBarChartProps as _StackedBarChartProps } from './plots/stacked-bar'
+import { StackedColumnChartProps as _StackedColumnChartProps } from './plots/stacked-column'
+import { StackedRoseChartProps as _StackedRoseChartProps } from './plots/stacked-rose'
+import { StepLineChartProps as _StepLineChartProps } from './plots/step-line'
+import { TreemapChartProps as _TreemapChartProps } from './plots/treemap'
+import { WaterfallChartProps as _WaterfallChartProps } from './plots/waterfall'
+import { WordCloudChartProps as _WordCloudChartProps } from './plots/word-cloud'
 
 // mini plots
-export { default as ProgressChart, ProgressChartProps } from './plots/progress'
-export {
-  default as RingProgressChart,
-  RingProgressChartProps,
-} from './plots/ring-progress'
-export { default as TinyAreaChart, TinyAreaChartProps } from './plots/tiny-area'
-export {
-  default as TinyColumnChart,
-  TinyColumnChartProps,
-} from './plots/tiny-column'
-export { default as TinyLineChart, TinyLineChartProps } from './plots/tiny-line'
+import { ProgressChartProps as _ProgressChartProps } from './plots/progress'
+import { RingProgressChartProps as _RingProgressChartProps } from './plots/ring-progress'
+import { TinyAreaChartProps as _TinyAreaChartProps } from './plots/tiny-area'
+import { TinyColumnChartProps as _TinyColumnChartProps } from './plots/tiny-column'
+import { TinyLineChartProps as _TinyLineChartProps } from './plots/tiny-line'
 
 // combo plots
-export { default as DualLineChart, DualLineChartProps } from './plots/dual-line'
-export {
-  default as ColumnLineChart,
-  ColumnLineChartProps,
-} from './plots/column-line'
-export {
-  default as GroupedColumnLineChart,
-  GroupedColumnLineChartProps,
-} from './plots/grouped-column-line'
-export {
-  default as StackedColumnLineChart,
-  StackedColumnLineChartProps,
-} from './plots/stacked-column-line'
+import { DualLineChartProps as _DualLineChartProps } from './plots/dual-line'
+import { ColumnLineChartProps as _ColumnLineChartProps } from './plots/column-line'
+import { GroupedColumnLineChartProps as _GroupedColumnLineChartProps } from './plots/grouped-column-line'
+import { StackedColumnLineChartProps as _StackedColumnLineChartProps } from './plots/stacked-column-line'
+
+export { default as AreaChart } from './plots/area'
+export type AreaChartProps = _AreaChartProps
+
+export { default as BarChart } from './plots/bar'
+export type BarChartProps = _BarChartProps
+
+export { default as BubbleChart } from './plots/bubble'
+export type BubbleChartProps = _BubbleChartProps
+
+export { default as BulletChart } from './plots/bullet'
+export type BulletChartProps = _BulletChartProps
+
+export { default as CalendarChart } from './plots/calendar'
+export type CalendarChartProps = _CalendarChartProps
+
+export { default as ColumnChart } from './plots/column'
+export type ColumnChartProps = _ColumnChartProps
+
+export { default as DensityChart } from './plots/density'
+export type DensityChartProps = _DensityChartProps
+
+export { default as DensityHeatmapChart } from './plots/density-heatmap'
+export type DensityHeatmapChartProps = _DensityHeatmapChartProps
+
+export { default as DonutChart } from './plots/donut'
+export type DonutChartProps = _DonutChartProps
+
+export { default as FanGaugeChart } from './plots/fan-gauge'
+export type FanGaugeChartProps = _FanGaugeChartProps
+
+export { default as FunnelChart } from './plots/funnel'
+export type FunnelChartProps = _FunnelChartProps
+
+export { default as GaugeChart } from './plots/gauge'
+export type GaugeChartProps = _GaugeChartProps
+
+export { default as GroupedBarChart } from './plots/grouped-bar'
+export type GroupedBarChartProps = _GroupedBarChartProps
+
+export { default as GroupedColumnChart } from './plots/grouped-column'
+export type GroupedColumnChartProps = _GroupedColumnChartProps
+
+export { default as GroupedRoseChart } from './plots/grouped-rose'
+export type GroupedRoseChartProps = _GroupedRoseChartProps
+
+export { default as HeatmapChart } from './plots/heatmap'
+export type HeatmapChartProps = _HeatmapChartProps
+
+export { default as HistogramChart } from './plots/histogram'
+export type HistogramChartProps = _HistogramChartProps
+
+export { default as LineChart } from './plots/line'
+export type LineChartProps = _LineChartProps
+
+export { default as LiquidChart } from './plots/liquid'
+export type LiquidChartProps = _LiquidChartProps
+
+export { default as MeterGaugeChart } from './plots/meter-gauge'
+export type MeterGaugeChartProps = _MeterGaugeChartProps
+
+export { default as PercentStackedAreaChart } from './plots/percent-stacked-area'
+export type PercentStackedAreaChartProps = _PercentStackedAreaChartProps
+
+export { default as PercentStackedBarChart } from './plots/percent-stacked-bar'
+export type PercentStackedBarChartProps = _PercentStackedBarChartProps
+
+export { default as PercentStackedColumnChart } from './plots/percent-stacked-column'
+export type PercentStackedColumnChartProps = _PercentStackedColumnChartProps
+
+export { default as PieChart } from './plots/pie'
+export type PieChartProps = _PieChartProps
+
+export { default as RadarChart } from './plots/radar'
+export type RadarChartProps = _RadarChartProps
+
+export { default as RangeBarChart } from './plots/range-bar'
+export type RangeBarChartProps = _RangeBarChartProps
+
+export { default as RangeColumnChart } from './plots/range-column'
+export type RangeColumnChartProps = _RangeColumnChartProps
+
+export { default as RingChart } from './plots/ring'
+export type RingChartProps = _RingChartProps
+
+export { default as RoseChart } from './plots/rose'
+export type RoseChartProps = _RoseChartProps
+
+export { default as ScatterChart } from './plots/scatter'
+export type ScatterChartProps = _ScatterChartProps
+
+export { default as StackedAreaChart } from './plots/stacked-area'
+export type StackedAreaChartProps = _StackedAreaChartProps
+
+export { default as StackedBarChart } from './plots/stacked-bar'
+export type StackedBarChartProps = _StackedBarChartProps
+
+export { default as StackedColumnChart } from './plots/stacked-column'
+export type StackedColumnChartProps = _StackedColumnChartProps
+
+export { default as StackedRoseChart } from './plots/stacked-rose'
+export type StackedRoseChartProps = _StackedRoseChartProps
+
+export { default as StepLineChart } from './plots/step-line'
+export type StepLineChartProps = _StepLineChartProps
+
+export { default as TreemapChart } from './plots/treemap'
+export type TreemapChartProps = _TreemapChartProps
+
+export { default as WaterfallChart } from './plots/waterfall'
+export type WaterfallChartProps = _WaterfallChartProps
+
+export { default as WordCloudChart } from './plots/word-cloud'
+export type WordCloudChartProps = _WordCloudChartProps
+
+// mini plots
+export { default as ProgressChart } from './plots/progress'
+export type ProgressChartProps = _ProgressChartProps
+
+export { default as RingProgressChart } from './plots/ring-progress'
+export type RingProgressChartProps = _RingProgressChartProps
+
+export { default as TinyAreaChart } from './plots/tiny-area'
+export type TinyAreaChartProps = _TinyAreaChartProps
+
+export { default as TinyColumnChart } from './plots/tiny-column'
+export type TinyColumnChartProps = _TinyColumnChartProps
+
+export { default as TinyLineChart } from './plots/tiny-line'
+export type TinyLineChartProps = _TinyLineChartProps
+
+// combo plots
+export { default as DualLineChart } from './plots/dual-line'
+export type DualLineChartProps = _DualLineChartProps
+
+export { default as ColumnLineChart } from './plots/column-line'
+export type ColumnLineChartProps = _ColumnLineChartProps
+
+export { default as GroupedColumnLineChart } from './plots/grouped-column-line'
+export type GroupedColumnLineChartProps = _GroupedColumnLineChartProps
+
+export { default as StackedColumnLineChart } from './plots/stacked-column-line'
+export type StackedColumnLineChartProps = _StackedColumnLineChartProps
