458a0764cff9bb518cf59d20f18e16a4e45c9ec0
kagawagao <kingsongao1221@gmail.com>
2020-07-01 09:47:51 +0800

    chore: ignore umi

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: '@opd/jest-preset-pangu',
   setupFiles: ['jest-canvas-mock'],
+  transformIgnorePatterns: ['/node_modules/', '/src/.umi/'],
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx}',
+    '!./**/*.d.ts',
+    '!src/.umi/**/*.ts',
+  ],
 }
