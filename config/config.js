d7cdc149f3900a90339bec6033316d7c41cf8192
kagawagao <kingsongao1221@gmail.com>
2023-01-29 15:31:56 +0800

    docs: fix vercel build

--- config/config.js
+++ config/config.js
@@ -1,3 +1,15 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+}
+
 export default {
   mode: 'site',
   title: 'G2Plot React',
